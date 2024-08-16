06c03469f239508b8726628d13ed217a893259a4
thanmaisai-asc <thanmai.sai@ascendion.com>
2024-08-16 17:02:54 +0530

    created store
    npm pkg(redux, react-redux, redux-devtools)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import { Provider } from 'react-redux'
+import store from '../src/redux/store'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <Provider store={store}>
       <h1 className='text-5xl'>Hello Developers</h1>
-    </>
+    </Provider>
   )
 }
 
